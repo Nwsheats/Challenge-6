af86b99f5f94ceb9d85687ce5276ab3366da7bbe
Nwsheats <nwsheats@gmail.com>
2022-10-04 23:28:09 -0500

    worked on Bootstrap, HTML, and added basic jQuery/JS variables

---
+++ script.js
@@ -0,0 +1,20 @@
+const Button = $('#btn');
+const recentSearch = $('#recent-search');
+const recentList = $('#recent-list');
+const locationToday = $('#location-today');
+const locationName = $('#location-name');
+const temp = $('#temperature');
+const wind = $('#wind');
+const humidity = $('#humidity');
+const forecast = $('#forecast');
+const day1 = $('#day-1');
+const day2 = $('#day-2');
+const day3 = $('#day-3');
+const day4 = $('#day-4');
+const day5 = $('#day-5');
+
+
+const today = moment().format("MM-DD-YYYY")
+
+const apiKey = "379288c134bd33ff0ca6a16b87f06183";
+
